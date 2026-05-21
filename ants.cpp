18d761d6cf35982f5bf790cae0c3817378b424a7
nicolas-riera <habbo.rieranicolas08@gmail.com>
2026-05-21 10:55:46 +0200

    feat: Added DFS path resolver and completed main run function

--- ants.cpp
+++ ants.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include "ants.hpp"
 
 short Ant::counter = 0;
@@ -10,8 +9,9 @@ Ant::Ant(){
 
 Ant::~Ant(){};
 
-Room::Room(short size){
+Room::Room(short size, std::string name){
     this->size = size;
+    this->name = name;
     std::vector<Ant*> newAnts;
     this->ants = newAnts;
 };
@@ -24,7 +24,7 @@ void Room::addAnt(Ant* ant){
     this->ants.push_back(ant);
 };
 
-Ant Room::extractAnt(){
+Ant* Room::extractAnt(){
     //check if room is empty for safety
     if (ants.empty())
     {
@@ -35,7 +35,7 @@ Ant Room::extractAnt(){
     //delete last element
     ants.pop_back();
     //return the value of the extracted ant
-    return *extractedAnt;
+    return extractedAnt;
 };
 
 Room::~Room(){
@@ -44,7 +44,53 @@ Room::~Room(){
     }
 };
 
-Anthill::Anthill(){};
+bool Room::DFSPath(Room* destination, std::unordered_set<Room*>& visited, Room* startRoom, Room*& nextRoom, std::unordered_map<Room*, int>& virtualAntCount) {
+    
+    // Exit condition
+    if (this == destination) {
+        return true;
+    }
+
+    visited.insert(this);
+
+    Room* bestNeighbor = nullptr;
+    int shortestPathLength = 1000000; // Insanely high number so the first path is the shortest one
+
+    for (Room* neighbor : this->adjacencies) {
+        if (visited.find(neighbor) == visited.end()) {
+            if (neighbor != destination && neighbor != startRoom && neighbor->size != -1) {
+                if (virtualAntCount[neighbor] >= neighbor->size) {
+                    continue;
+                }
+            }
+
+            // We start a new search branch from the neighbor to find the shortest past
+            Room* dummyNext = nullptr; // Dummy to not override the actual nextRoom
+            std::unordered_set<Room*> branchVisited = visited;
+            
+            if (neighbor->DFSPath(destination, branchVisited, startRoom, dummyNext, virtualAntCount)) {
+                int currentLength = static_cast<int>(branchVisited.size());
+                if (currentLength < shortestPathLength) {
+                    shortestPathLength = currentLength;
+                    bestNeighbor = neighbor;
+                }
+            }
+        }
+    }
+
+    // bestNeibor become the nextRoom to move for the ant
+    if (bestNeighbor != nullptr) {
+        if (this == startRoom) {
+            nextRoom = bestNeighbor;
+        }
+        return true;
+    }
+
+    // No path found (all adjacencies room are full), the ant won't move
+    return false;
+}
+
+Anthill::Anthill() : stepCounter(0) {}
 
 Anthill::~Anthill(){
     for (Room* room : this->rooms) {
@@ -67,7 +113,7 @@ void Anthill::initAnthill(std::vector<std::vector<short>> roomLinking, std::vect
         Room* room;
         if (i == 0) {
             // V room
-            room = new Room(roomSizes[i]);
+            room = new Room(roomSizes[i], "V");
             // Ants creation
             room->ants.reserve(antAmount);
             for (size_t j = 0; j < antAmount; j++)
@@ -77,10 +123,10 @@ void Anthill::initAnthill(std::vector<std::vector<short>> roomLinking, std::vect
             }
         } else if (i == roomSizes.size()-1) {
             // D room
-            room = new Room(roomSizes[i]);
+            room = new Room(roomSizes[i], "D");
         } else {
             // regular rooms
-            room = new Room(roomSizes[i]);
+            room = new Room(roomSizes[i], "S" + std::to_string(i));
         }
         this->rooms.push_back(room);
     }
@@ -93,57 +139,75 @@ void Anthill::initAnthill(std::vector<std::vector<short>> roomLinking, std::vect
             if (this->roomLinking[i][j] == 1)
             {
                 this->rooms[i]->addToAdjacencies(this->rooms[j]);
-                
-                this->rooms[j]->addToAdjacencies(this->rooms[i]);
             }
         }
     }
     
-    this->printDebug();
+    // this->printDebug();
     
 };
 
-void Anthill::run(){
-    // To complete
-};
+// Main Resolver
+void Anthill::run() {
+    Room* end = this->rooms[this->rooms.size() - 1];
+    bool globalActivity = true;
 
-// Debugging only
-void Anthill::printDebug() {
-    for (size_t i = 0; i < this->rooms.size(); i++) {
+    while (globalActivity) {
+        globalActivity = false;
         
-        std::string roomName;
-        if (i == 0) roomName = "V";
-        else if (i == this->rooms.size() - 1) roomName = "D";
-        else roomName = "S" + std::to_string(i);
-
-        std::cout << "Index [" << i << "] (" << roomName << ") : "
-                  << "Max Size: " << this->roomSizes[i]
-                  << " | Ants: ";
+        std::vector<std::pair<Room*, Room*>> movesToExecute;
+        std::unordered_map<Room*, int> virtualAntCount;
         
-        if (i == 0) {
-            for (short j = 0; j < this->antAmount; j++) {
-                std::cout << j << " ";
-            }
-        } else {
-            std::cout << "None";
+        // Create a virtual ant count in rooms to properly check if rooms are empty
+        for (Room* room : this->rooms) {
+            virtualAntCount[room] = static_cast<int>(room->ants.size());
         }
 
-        std::cout << " | Adjacencies: ";
-        if (this->rooms[i]->adjacencies.empty()) {
-            std::cout << "None";
-        } else {
-            for (size_t n = 0; n < this->rooms[i]->adjacencies.size(); n++) {
-                for (size_t k = 0; k < this->rooms.size(); k++) {
-                    if (this->rooms[i]->adjacencies[n] == this->rooms[k]) {
-                        if (k == 0) std::cout << "V ";
-                        else if (k == this->rooms.size() - 1) std::cout << "D ";
-                        else std::cout << "S" << k << " ";
-                        break;
+        // Go through all rooms
+        for (int i = static_cast<int>(this->rooms.size()) - 2; i >= 0; i--) {
+            Room* currentRoom = this->rooms[i];
+            
+            if (currentRoom->ants.empty()) {
+                continue;
+            }
+
+            size_t initialAntsInRoom = currentRoom->ants.size();
+
+            // Check the movability of all ants
+            for (size_t k = 0; k < initialAntsInRoom; k++) {
+                std::unordered_set<Room*> visited;
+                Room* nextStep = nullptr;
+
+                if (currentRoom->DFSPath(end, visited, currentRoom, nextStep, virtualAntCount)) {
+                    if (nextStep) {
+                        // Add the move to the move list and adjust the virtual ant count
+                        movesToExecute.push_back({currentRoom, nextStep});
+                        virtualAntCount[currentRoom]--;
+                        virtualAntCount[nextStep]++;
+                        globalActivity = true;
                     }
+                } else {
+                    break;
                 }
             }
         }
 
-        std::cout << std::endl;
+        // Make the actual moves when all the planning is done
+        if (!movesToExecute.empty()) {
+            this->stepCounter++;
+            std::cout << "--- Step " << this->stepCounter << " ---" << std::endl;
+            
+            for (auto& move : movesToExecute) {
+                Room* src = move.first;
+                Room* dst = move.second;
+                
+                // Extract the ant from the source room and move it to the next room
+                Ant* travellingAnt = src->extractAnt();
+                dst->addAnt(travellingAnt);
+                
+                std::cout << "Ant #" << travellingAnt->getId() << " : " 
+                          << src->name << " -> " << dst->name << std::endl;
+            }
+        }
     }
-}
+};
